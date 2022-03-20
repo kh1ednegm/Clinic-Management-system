63515651d8a978f246a878c8d43e66ea253246c4
kh1ednegm <20912017100081@fci.zu.edu.eg>
2022-03-20 17:43:26 +0200

    khaled-1

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+
+
+
+const server = express()
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect("mongodb://localhost:27017/CMS").then(()=>{
+    console.log("Connected to DB")
+    server.listen(PORT,()=>{
+        console.log(`Im listening to port `+ PORT)
+        
+    })
+}).catch((error)=>{
+    console.log("Conncetion faild")
+})
+
+
+server.use(morgan(":url :method"))
+server.use(bodyParser.json())
 
 
 
 
-const app = express()
 
 
 
 
 
-app.listen(process.env.PORT,()=>{
-    console.log("I'm listening")
+
+
+
+
+server.use((error,request,response,next)=>{
+
+    error.status = error.status || 500
+    response.status(error.status).send(""+error)
 })
