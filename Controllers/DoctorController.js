a19a75397007ecb61d6a3a8507aca95a07216b93
AhmedHamdyHassan <fairytailuzumaki2@gmail.com>
2022-03-23 01:27:32 +0200

    Finished Doctor Operations

--- Controllers/DoctorController.js
+++ Controllers/DoctorController.js
@@ -1,38 +1,107 @@
 const mongoose = require('mongoose')
 
-const Doctor=require('../Models/DoctorModel');
-const User=require('../Models/UserModel');
-const bcrypt=require('bcrypt');
+const Doctor = require('../Models/DoctorModel');
+const User = require('../Models/UserModel');
+const bcrypt = require('bcrypt');
 
 
-
-exports.AddDoctor = async (request,response)=>{
-
-    Doctor.create({
-        'name':request.body['name'],
-        'birthday':request.body['birthday'],
-        'gender':request.body['gender'],
-        'address':request.body['address'],
-        'phoneno':request.body['phoneno'],
-        'specialisation':request.body['specialisation']
-    }).then(async(doctor)=>{
+//Add Doctor
+exports.AddDoctor = async (request, response) => {
+    try {
+        console.log("Iam here")
+        let doctor = await Doctor.create({
+            'name': request.body['name'],
+            'birthday': request.body['birthday'],
+            'gender': request.body['gender'],
+            'address': request.body['address'],
+            'phoneno': request.body['phoneno'],
+            'specialisation': request.body['specialisation']
+        })
         const salt = await bcrypt.genSalt(10);
         const hashedpassword = await bcrypt.hash(request.body['password'], salt);
-        User.create({
-            'email':request.body['email'],
-            'password':hashedpassword,
-            'userType':'doctor',
-            'UserID':doctor._id,
-        }).then((user)=>{
-            if(user){
-                response.status(200).send({message:"OK"})
-            }
-            else{
-                response.status(400).send({error:"Faild"})
-            }
-        });
-    });
-    
+        let user = await User.create({
+            'email': request.body['email'],
+            'password': hashedpassword,
+            'userType': 'doctor',
+            'UserID': doctor._id,
+        })
+        if (user) {
+            response.status(200).send({ message: "OK" })
+        }
+        else {
+            response.status(400).send({ error: "Faild" })
+        }
+    } catch (err) {
+        response.status(400).send({ error: "Faild" })
+    }
 }
 
+//Delete Doctor
+exports.DeleteDoctor = async (request,response)=>{
+    try {
+        let body = request.body
 
+        let doctor = await Doctor.findByIdAndDelete(body._id)
+        if(doctor){
+            response.status(200).send({message:"OK"})
+        }
+        else{
+            response.status(400).send({error:"Doctor is Not Found"})
+        }
+
+    } catch (err) {
+        response.status(400).send({error:"Failed"})
+    }
+}
+
+// Get Doctor by ID
+exports.GetDoctorByID = async (request,response)=>{
+    try {
+        let doctor = await Doctor.findById(request.body._id)
+        if(doctor){
+            response.status(200).send({message:"OK",data:doctor})
+        }
+        else{
+            response.status(400).send({error:"Doctor is Not Found"})
+        }
+    } catch (err) {
+        response.status(400).send({error:"Failed"})
+    }
+}
+
+// GET all Doctors
+exports.GetAllDoctors = async (request,response)=>{
+    try {
+        let doctors = await Doctor.find({})
+        if(doctors){
+            response.status(200).send({message:"OK",data:doctors})
+        }
+        else{
+            response.status(400).send({error:"Doctors Not Found"})
+        }
+    } catch (err) {
+        response.status(400).send({error:"Failed"})
+    }
+} 
+
+// Edit Employee
+exports.EditDoctor = async (request,response)=>{
+    try {
+        let doctor = await Doctor.findByIdAndUpdate({_id:request.body._id},{
+            'name': request.body['name'],
+            'birthday': request.body['birthday'],
+            'gender': request.body['gender'],
+            'address': request.body['address'],
+            'phoneno': request.body['phoneno'],
+            'specialisation': request.body['specialisation']
+        })
+        if (doctor) {
+            response.status(200).send({message:"OK",data:doctor})
+        }
+        else{
+            response.status(400).send({error:"Doctors Not Found"})
+        }
+    } catch (err) {
+        response.status(400).send({error:"Failed"})
+    }
+}
--- Routes/DoctorRoute.js
+++ Routes/DoctorRoute.js
@@ -7,9 +7,41 @@ const auth = require('../middleware/auth')
 
 
 //Add a new Doctor
-router.post('/doctor',auth.checkAdmin,(req,res)=>{
+router.post('/doctor/add',(req,res)=>{
     DoctorController.AddDoctor(req,res)
 });
 
+//Delete Doctor
+router.delete('/doctor/delete',(request,response)=>{
+    console.log("Iam here")
+    DoctorController.DeleteDoctor(request,response)
+})
 
+//Get Doctor ID
+router.get('/doctor/byId',(request,response)=>{
+    DoctorController.GetDoctorByID(request,response)
+})
+
+//Get All Doctors
+router.get('/doctor/all',(request,response)=>{
+    console.log("hghghhghg");
+    DoctorController.GetAllDoctors(request,response)
+})
+
+//Edit Doctor
+router.put('/doctor/edit',(request,response)=>{
+    DoctorController.EditDoctor(request,response)
+})
+//,auth.checkAdmin
+/*{
+    "_id": "6238a48bbd9dca7904058899"
+    "name":"Ahmed Hamdy",
+    "birthday":"5/5/1999",
+    "gender":"m",
+    "address":"Assiut",
+    "phoneno":1027866191,
+    "specialisation":"Diabates",
+    "email":"fairytailuzumaki2@gmail.com",
+    "password":"Kirito1151999"
+}*/
 module.exports= router;
